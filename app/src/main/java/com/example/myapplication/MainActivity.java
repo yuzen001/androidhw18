bfc15748adcc19aff018b656aaba6d063f61a8a1
yuzen001 <jamapaice1668@gmail.com>
2019-12-27 13:05:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListAdapter listAdapter;
+    private TextView textView;
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.textview);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                new String[]{"item 1", "item 2", "item 3","item 4"});
+        listView.setAdapter(listAdapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE); //設定列表選擇模式(單選)
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                // TODO Auto-generated method stub
+                textView.setText("你選擇了: " + ((TextView) view).getText());
+            }
+        });
+    }
+}
